04ce11c0744ae3ab838f5f24c04808e404221c24
George Fourtounis <gfour@di.uoa.gr>
2021-01-21 17:28:19 +0200

    Add Kotlin test.

--- src/test/java/org/clyze/source/irfitter/test/TestMapper.java
+++ src/test/java/org/clyze/source/irfitter/test/TestMapper.java
@@ -10,22 +10,37 @@ import org.junit.jupiter.api.Test;
 //import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMapper {
+
     /**
-     * Test that a command-line invocation will not crash and the output
-     * directory is not empty.
+     * Test that a command-line invocation will not crash on
+     * Java/Groovy sources and the output directory is not empty.
      */
     @Test
-    void test() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        String clueCommonJar = classLoader.getResource("clue-common-3.24.1.jar").getFile();
-        System.out.println("clueCommonJar: " + clueCommonJar);
-        String clueCommonSourcesJar = classLoader.getResource("clue-common-3.24.1-sources.jar").getFile();
-        System.out.println("clueCommonSourcesJar: " + clueCommonSourcesJar);
-        String outDir = "build/test-out";
-        FileUtils.deleteDirectory(new File(outDir));
-        String[] args = new String[]{"--ir", clueCommonJar, "--source", clueCommonSourcesJar, "--out", outDir, "--json"};
-        Main.main(args);
-
+    void testJavaAndGroovy() throws IOException {
+        String outDir = "build/test-out-java-groovy";
+        generateJson("clue-common-3.24.1.jar", "clue-common-3.24.1-sources.jar", outDir);
         assert (new File(outDir).listFiles() != null);
+    }
+
+    /**
+     * Test that a command-line invocation will not crash on
+     * Kotlin sources and the output directory is not empty.
+     */
+    @Test
+    void testKotlin() throws IOException {
+        String outDir = "build/test-out-kotlin";
+        generateJson("noarg-compiler-plugin.jar", "noarg-compiler-plugin-sources.zip", outDir);
+        assert (new File(outDir).listFiles() != null);
+    }
+
+    void generateJson(String jarRes, String sourcesJarRes, String outDir) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        String jar = classLoader.getResource(jarRes).getFile();
+        System.out.println("jar: " + jar);
+        String sourcesJar = classLoader.getResource(sourcesJarRes).getFile();
+        System.out.println("sourcesJar: " + sourcesJar);
+        FileUtils.deleteDirectory(new File(outDir));
+        String[] args = new String[]{"--ir", jar, "--source", sourcesJar, "--out", outDir, "--json"};
+        Main.main(args);
     }
 }
